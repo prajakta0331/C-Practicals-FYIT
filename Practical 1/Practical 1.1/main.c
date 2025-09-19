f5a0d9f70f35fa1f09e6a68996d336fe77355278
Prajakta Sangulkar <prajaktasangulkar0@gmail.com>
2025-09-19 14:46:45 +0530

    Add files via upload

---
+++ Practical 1/Practical 1.1/main.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int a,b,c,greatest;
+
+    printf("ENTER THREE NUMBER: \n");
+    scanf("%d %d %d" , &a,&b,&c);
+
+    greatest = (a > b && a > c )? a : ((b > c) ? b : c );
+
+    printf("greatest number is = %d \n" , greatest);
+
+    return 0;
+}
